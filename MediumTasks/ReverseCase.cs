ef7f0da5a664d232ca5ce99286a15d3846aa44d5
Timclewis <38727434+Timclewis@users.noreply.github.com>
2021-02-24 21:06:06 +0100

    Renamed program

---
+++ MediumTasks/ReverseCase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MediumTasks
+{
+    class ReverseCase
+    {
+        public static string Reverse(string str)
+        {
+            string result = string.Empty;
+            char[] strArray = str.ToCharArray();
+            foreach (char i in strArray)
+            {
+                if (char.IsLower(i))
+                    result += Char.ToUpper(i);
+                else
+                    result += Char.ToLower(i);                   
+            }
+
+            Console.WriteLine(result);
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string str = "Happy Birthday";
+            Console.WriteLine( Reverse(str)); 
+        }
+    }
+}
