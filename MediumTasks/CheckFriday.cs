a62e7740de5f875478f9cb8bf20813953c20ee2b
Timclewis <38727434+Timclewis@users.noreply.github.com>
2021-02-25 02:57:15 +0100

    Friday 13th task

---
+++ MediumTasks/CheckFriday.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace MediumTasks
+{
+    public class CheckFriday
+    {
+        public static bool Friday(int month, int year)
+        {
+            var dt = new DateTime(year, month, 13);
+            if (dt.DayOfWeek == DayOfWeek.Friday)
+                return true;
+
+            else
+                return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int month = 1, year = 1985;
+            Console.WriteLine(Friday(month,year));
+        }
+
+    }
+}
